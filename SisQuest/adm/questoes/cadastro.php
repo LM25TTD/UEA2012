9b3402c326ae7bed4c6262a19e61fd0e02ead846
Kevin Gustavo <kevingmq@gmail.com>
2012-05-20 23:28:49 -0400

    Commit com jquery funcionando para assunto area e disciplina.

--- SisQuest/adm/questoes/cadastro.php
+++ SisQuest/adm/questoes/cadastro.php
@@ -6,6 +6,8 @@ protegePagina();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="../../scripts/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="../../scripts/comboAreaDisciplinaAssunto.js"></script>
 <title>SisQuest</title>
 <link rel="stylesheet" href="../../styles.css" type="text/css" />
 <!--
